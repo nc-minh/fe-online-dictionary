bf37f569ecbe31f12223efe812fd13cd6620f3da
nc-minh <nguyencongminh.official@gmail.com>
2022-02-10 17:53:46 +0700

    :sparkles: update check-register -> client

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 function checkLogin(){
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
-    var alertUsername = document.getElementById('alert-username')
+    let alertUsername = document.getElementById('alert-username')
     let alertPassword = document.getElementById('alert-password')
 
-    const regex = /[a-zA-Z0-9._-]{5,20}/g
+    const regex = /^\w+$/g
     const check = regex.test(username)
 
     console.log(check)
 
-    if(username.length < 5){
-        alertUsername.innerText = 'Tài khoản phải lớn hơn 6 kí tự!'
+    if(username.length < 5 || username.length > 20){
+        alertUsername.innerText = 'Tài khoản phải lớn hơn 5 kí tự!'
         alertUsername.style.display = 'block'
     }else if(!check){
         alertUsername.innerText = 'Tài khoản không chứa kí tự đặc biệt!'
         alertUsername.style.display = 'block'
     }else if(password.length < 5){
-        alertPassword.innerText = 'Mật khẩu phải lớn hơn 6 kí tự!'
+        alertPassword.innerText = 'Mật khẩu phải lớn hơn 5 kí tự!'
         alertUsername.style.display = 'none'
         alertPassword.style.display = 'block'
     }else{
@@ -26,4 +26,45 @@ function checkLogin(){
         return true
     }
 
+}
+
+function checkRegister(){
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+    const rePassword = document.getElementById('re-password').value
+    let alertUsername = document.getElementById('alert-username')
+    let alertPassword = document.getElementById('alert-password')
+    let alertRepassword = document.getElementById('alert-repassword')
+
+    const regex = /^\w+$/g
+    const check = regex.test(username)
+    console.log(check)
+
+    if(username.length < 5 || username.length > 20){
+        alertUsername.innerText = 'Tài khoản phải lớn hơn 5 kí tự!'
+        alertUsername.style.display = 'block'
+        alertPassword.style.display = 'none'
+        alertRepassword.style.display = 'none'
+    }else if(!check){
+        alertUsername.innerText = 'Tài khoản không chứa kí tự đặc biệt!'
+        alertUsername.style.display = 'block'
+        alertPassword.style.display = 'none'
+        alertRepassword.style.display = 'none'
+    }else if(password.length < 5){
+        alertUsername.innerText = 'Mật khẩu phải lớn hơn 5 kí tự!'
+        alertUsername.style.display = 'none'
+        alertPassword.style.display = 'block'
+        alertRepassword.style.display = 'none'
+    }else if(password != rePassword){
+        alertRepassword.innerText = 'Mật khẩu nhập lại không đúng!'
+        alertUsername.style.display = 'none'
+        alertPassword.style.display = 'none'
+        alertRepassword.style.display = 'block'
+    }else{
+        alertUsername.style.display = 'none'
+        alertPassword.style.display = 'none'
+        alertRepassword.style.display = 'none'
+        console.log('done')
+        return true
+    }
 }
